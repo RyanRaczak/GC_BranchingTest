52965901875083d777ef0e1075c4645d0ea60b30
Ryan Raczak <ryanraczak@rocketmortgage.com>
2021-11-03 14:46:39 -0400

    testing branching

---
+++ GC_BranchingTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GC_BranchingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is initial branch");
+        }
+    }
+}
